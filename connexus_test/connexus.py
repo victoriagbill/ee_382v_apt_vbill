3c6d6e81b84c4639c6f7bc247fb30acbd2ccce91
Steve Rutherford <rutherford.steve@yahoo.com>
2014-09-26 14:33:54 -0500

    Search/Trending
    Added Deque and Templates for Search/Trending

--- connexus_test/connexus.py
+++ connexus_test/connexus.py
@@ -26,6 +26,19 @@ JINJA_ENVIRONMENT = jinja2.Environment(
   autoescape=True)
 
 
+'''
+DEQUE INIT'S -SR
+'''
+from collections import deque
+from datetime import datetime, timedelta
+
+#A list of queues, where each queue holds the timestapmps of when each stream is accessed
+recent = []
+index_of_new_stream = 0
+
+
+
+
 class MainPage(webapp2.RequestHandler):
 	#main page = login, should check for login then dump to manage page?
   def get(self):
@@ -104,6 +117,36 @@ class Create(webapp2.RequestHandler):
   	}
 		template = JINJA_ENVIRONMENT.get_template('create_stream.html')
 		self.response.write(template.render(template_values))
+
+
+		'''
+		Each time a stream is created, create a queue for that stream -SR
+		'''
+		recent[index_of_new_stream] = (streamName, deque() )
+		index_of_new_stream += 1
+
+
+class Delete(webapp2.RequestHandler):
+	def get(self):
+		user = users.get_current_user()
+		if user:
+			url = users.create_logout_url('/')
+			url_linktext = 'Logout'
+		else:
+			url = users.create_login_url(self.request.uri)
+			url_linktext = 'Login'
+
+		template_values = {
+      'url': url,
+      'url_linktext': url_linktext,
+			'upload_url': upload_url,
+  		}
+		template = JINJA_ENVIRONMENT.get_template('create_stream.html')
+		self.response.write(template.render(template_values))
+
+		#TODO remove stream from store?
+		index_of_new_stream -= 1
+
 
 #class CreateStreamHandler(webapp2.RequestHandler):
 	#def post(self):
@@ -192,6 +235,19 @@ class ViewSingle(webapp2.RequestHandler):
 		# the create page serves as the MainHandler for the create stream UploadHandler, ServeHandler
 		upload_url = blobstore.create_upload_url('/upload')
 		user = users.get_current_user()
+
+		'''
+		UPDATE VIEWS
+		Each time a stream has been viewed
+		'''
+		print "time viewed" + timestamp
+		timestamp =  datetime.now()
+		currentTIme = datetime.now()
+		streamIndex = recent.index(streamName) #don't need to give index from deque, right?
+		#streamIndex is where the tuple of name/deque is and [1] is the deque itself
+		recent[streamIndex][1].append(timestamp)
+
+
 		if user:
 			url = users.create_logout_url('/')
 			url_linktext = 'Logout'
@@ -229,11 +285,42 @@ class Search(webapp2.RequestHandler):
 		template = JINJA_ENVIRONMENT.get_template('search.html')
 		self.response.write(template.render(template_values))
 
+'''
+START OF STEVE'S FUNCTIONS
+'''
+
+from difflib import SequenceMatcher
+
 class SearchStreams(webapp2.RequestHandler):
 	def post(self):
 		search_data = self.request.get('cxus_search')
 		print search_data
-		self.redirect('/search')
+
+		search_list = []
+
+		#TODO streamName/streamURL/stream tag names and structure
+		for streamName in streamDict:
+			search_list.append( SequenceMatcher(None, search_data, streamName).ratio()), (streamName, streamURL) )
+	
+		for tag in StreamDict:
+			search_list.append( SequenceMatcher(None, search_data, tag).ratio()), (streamName, streamURL) )
+
+		unique_search_list = set(search_list)
+
+		unique_search_list.sort
+
+		#return stream names/urls of top 5 streams
+		top_five_results = unique_search_list[:5][0]
+
+		if len(unique_search_list) > 0:
+				template_values['streams'] = unique_search_list[:5][0]
+			else:
+				template_values['command'] = 'No matching streams found'
+
+		template = JINJA_ENVIRONMENT.get_template('search_results.html')
+		self.response.write(template.render(template_values))
+			
+
 
 class Trending(webapp2.RequestHandler):
 	def get(self):
@@ -251,6 +338,83 @@ class Trending(webapp2.RequestHandler):
 		template = JINJA_ENVIRONMENT.get_template('trending.html')
 		self.response.write(template.render(template_values))
 
+		trending_streams = getTrends()
+		if len(trending_streams) > 0:
+			template_values['streams'] = trending_streams
+		else:
+			template_values['command'] = 'No trending streams found'
+
+		#TODO DISPLAY THE TRENDING STREAMS
+
+class TrendingShort(webapp2.RequestHandler):
+	def post(self):
+		freshenTrends()
+		if buttonPosition == short_update:
+			sendTrends() 
+
+class TrendingHourly(webapp2.RequestHandler):
+	def post(self):
+		#CHECK LAST UPDATED TREND TIME BUTTON
+		if buttonPosition == hourly_update:
+			sendTrends() 
+
+
+class TrendingDaily(webapp2.RequestHandler):
+	def post(self):
+		#CHECK LAST UPDATED TREND TIME BUTTON
+		if buttonPosition == daily_update:
+			sendTrends() 
+
+
+#Called every 5 minutes to clean out old timestamps 
+def freshenTrends():
+	'''
+	Clear times over an hour
+	'''
+
+	#are python queues indexed?
+	for stream in streamDict
+		index = 0
+		while if (timestamp - recent[stream][1][index]) > timedelta(hours = 1):
+			recent[stream[1].popleft()
+			index += 1
+
+	return
+
+
+#returns the top 3 trends
+def getTrends():
+	popStreams []
+	for i in recent:
+		popStreams[i] = len(recent[0][1])
+
+	popStreams.sort()
+
+	topStreams[] = (popStreams[0] : popStreams[3])
+
+	return topStreams
+
+
+def sendTrends():
+	trending_streams = getTrends()
+
+	#TODO FIX SENDING EMAIL
+	message = mail.EmailMessage(sender=user.email(), subject="Trending Update")
+	message.to = user.email
+	message.body = "CONNEXUS.US\n" +
+		"Daily Trending Report\n" +
+		"------------------------------------------\n" +
+		"1. " + trending_streams[0] + "\n" +
+		"2. " + trending_streams[1] + "\n" +
+		"3. " + trending_streams[2] + "\n"
+		#"Update Trending Prefrerences (Link)"
+	message.send()
+	self.redirect('/')
+
+
+'''
+EBD IF STEVE'S CHANGES
+'''
 class Social(webapp2.RequestHandler):
 	def get(self):
 		user = users.get_current_user()
@@ -316,6 +480,9 @@ application = webapp2.WSGIApplication([
 	('/search', Search),
 	('/searchstreams', SearchStreams),
 	('/trending', Trending),
+	('/trendingShort', TrendingShort),
+	('/TrendingHourly', TrendingHourly),
+	('/trendingDaily', TrendingDaily),
 	('/social', Social),
 	('/upload', UploadHandler),
 	('/serve/([^/]+)?', ServeHandler),
